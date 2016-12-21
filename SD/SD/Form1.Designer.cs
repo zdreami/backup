17e2559b1f66e5920c06f0fa930e099cdc913733
zdreami <zdream486@gmail.com>
2016-12-21 22:52:18 +0800

    Add files via upload

---
+++ SD/SD/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Hour = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Minutes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Hour
+            // 
+            this.Hour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Hour.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Hour.FormattingEnabled = true;
+            this.Hour.IntegralHeight = false;
+            this.Hour.ItemHeight = 30;
+            this.Hour.Items.AddRange(new object[] {
+            "00",
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09"});
+            this.Hour.Location = new System.Drawing.Point(12, 17);
+            this.Hour.Name = "Hour";
+            this.Hour.Size = new System.Drawing.Size(71, 38);
+            this.Hour.TabIndex = 0;
+            this.Hour.SelectedIndexChanged += new System.EventHandler(this.Hour_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(212, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "关机";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(89, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "：";
+            // 
+            // Minutes
+            // 
+            this.Minutes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Minutes.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Minutes.FormattingEnabled = true;
+            this.Minutes.IntegralHeight = false;
+            this.Minutes.Location = new System.Drawing.Point(120, 17);
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Size = new System.Drawing.Size(71, 38);
+            this.Minutes.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(348, 73);
+            this.Controls.Add(this.Minutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Hour);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "关机";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Hour;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Minutes;
+
+    }
+}
+
---
+++ SD/SD/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace SD
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool shut = false;
+        public string runcmd(string command)
+        {
+            //实例一个Process类，启动一个独立进程
+            Process p = new Process();
+            //设定程序名
+            p.StartInfo.FileName = "cmd.exe";
+            //设定程式执行参数  
+            p.StartInfo.Arguments = "/c " + command;
+            //关闭Shell的使用
+            p.StartInfo.UseShellExecute = false;   
+            //重定向标准输入     
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            //设置不显示窗口
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            return p.StandardOutput.ReadToEnd();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cmdcode;
+            if (Hour.SelectedItem == null || Minutes.SelectedItem == null)
+            {
+                return;
+            }
+            if (shut == false)
+            {
+                int nowH = DateTime.Now.Hour;
+                int nowM = DateTime.Now.Minute;
+                int nowS = DateTime.Now.Second;
+                int aimH = int.Parse(Hour.SelectedItem.ToString());
+                int aimM = int.Parse(Minutes.SelectedItem.ToString()); 
+                if (aimM < nowM)
+                {
+                    aimM += 60;
+                    aimH--;
+                }
+                if (aimH < nowH)
+                {
+                    aimH += 24;
+                }
+                int time = (aimH - nowH) * 60 * 60 + (aimM - nowM) * 60-nowS;
+                cmdcode="shutdown.exe -s -t "+ time.ToString();
+                //System.Diagnostics.Process.Start("cmd", "/c start/b shutdown.exe -s -t " + time.ToString());
+                button1.Text = "取消";
+                shut = true;
+            }
+            else
+            {
+                cmdcode="shutdown -a";
+                button1.Text = "关机";
+                shut = false;
+            }
+            runcmd(cmdcode);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //小时
+            for (int i = 10; i < 24; i++)
+            {
+                if(i<10)
+                    Hour.Items.Add("0" + i.ToString());
+                else
+                    Hour.Items.Add(i.ToString());
+            }
+
+            //分钟
+            for (int i = 0; i < 60; i+=5)
+            {
+                if(i<10)
+                    Minutes.Items.Add("0" + i.ToString());
+                else
+                    Minutes.Items.Add(i.ToString());
+            }
+        }
+
+        private void Hour_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
